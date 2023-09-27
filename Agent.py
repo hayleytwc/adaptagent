5105b14baaa536a903877fc86168dce462388853
hayleytwc <137235861+hayleytwc@users.noreply.github.com>
2023-09-28 12:34:53 +1300

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -8,14 +8,29 @@ api_key = st.secrets["openai"]["api_key"]
 # Initialize the OpenAI API client with the retrieved API key
 openai.api_key = api_key
 
+# Define a workplace strategy knowledge base
+workplace_strategy_knowledge = """
+Workplace strategy refers to the long-term planning and design of a physical workspace to optimize productivity, employee well-being, and organizational goals. In the UK, workplace strategies have evolved to accommodate flexible working arrangements, sustainability initiatives, and technology integration.
+
+Key aspects of workplace strategy in the UK include:
+1. Flexible Work Arrangements: Embracing remote and hybrid work models, hot-desking, and flexible scheduling to adapt to changing work dynamics.
+2. Well-being: Prioritizing employee health and well-being through ergonomic design, access to natural light, and wellness programs.
+3. Sustainability: Implementing eco-friendly practices, energy-efficient buildings, and reducing the carbon footprint.
+4. Technology Integration: Leveraging digital tools for collaboration, data analytics, and smart office solutions.
+5. Employee Engagement: Fostering a sense of community, collaboration, and employee engagement through the workspace design.
+
+Keep in mind that workplace strategy should align with your organization's unique goals and culture.
+"""
+
 def generate_response(prompt):
     completions = openai.Completion.create(
         engine="text-davinci-003",
-        prompt=prompt,
+        prompt=prompt + workplace_strategy_knowledge,
         max_tokens=1024,
         n=1,
         stop=None,
         temperature=0.4,
+        language="en_GB"
     )
     return completions.choices[0].text.strip()
 
