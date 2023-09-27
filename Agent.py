c5bc6fb17527233b8e493d0f881ff0c75893b758
hayleytwc <137235861+hayleytwc@users.noreply.github.com>
2023-09-27 20:49:00 +1300

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -1,6 +1,7 @@
 import streamlit as st
 import openai
 import toml
+import nltk
 
 # Access the OpenAI API key from st.secrets
 api_key = st.secrets["openai"]["api_key"]
@@ -15,8 +16,8 @@ st.write("Chat with the Workplace Strategy Consultant!")
 user_input = st.text_input("You:", "")
 if st.button("Send"):
     if user_input.strip() != "":
-        # Call the GPT-3 API to get a response
-        prompt = f"As a workplace strategy consultant, how can I assist you with '{user_input}'?"
+        # Call the API to get a response
+        prompt = f"As a workplace strategy consultant, tell me '{user_input}'?"
         temperature = 0.4
         response = openai.Completion.create(
             engine="babbage-002",
@@ -26,3 +27,16 @@ if st.button("Send"):
         )
         bot_response = response.choices[0].text.strip()
         st.text("AdaptAgent: " + bot_response)
+
+# Extract the AI response
+ai_response = response.choices[0].text
+
+# Post-process the response to truncate it at the end of a sentence
+sentences = nltk.sent_tokenize(ai_response)
+if len(sentences) > 0:
+    truncated_response = ' '.join(sentences[:-1])  # Keep all sentences except the last one
+else:
+    truncated_response = ai_response  # No sentences found, use the original response
+
+# Display the truncated response
+print("Truncated Response:", truncated_response)
