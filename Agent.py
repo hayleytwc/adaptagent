1a177a9c6ee996e26b1c7b54cbf7eb6d8e93b8b3
hayleytwc <137235861+hayleytwc@users.noreply.github.com>
2023-09-30 20:45:11 +1300

    Update Agent.py

--- Agent.py
+++ Agent.py
@@ -10,16 +10,10 @@ openai.api_key = api_key
 
 # Define a workplace strategy knowledge base
 workplace_strategy_knowledge = """
-Workplace strategy refers to the long-term planning and design of a physical workspace to optimize productivity, employee well-being, and organizational goals. In the UK, workplace strategies have evolved to accommodate flexible working arrangements, sustainability initiatives, and technology integration.
-
-Key aspects of workplace strategy in the UK include:
-1. Flexible Work Arrangements: Embracing remote and hybrid work models, hot-desking, and flexible scheduling to adapt to changing work dynamics.
-2. Well-being: Prioritizing employee health and well-being through ergonomic design, access to natural light, and wellness programs.
-3. Sustainability: Implementing eco-friendly practices, energy-efficient buildings, and reducing the carbon footprint.
-4. Technology Integration: Leveraging digital tools for collaboration, data analytics, and smart office solutions.
-5. Employee Engagement: Fostering a sense of community, collaboration, and employee engagement through the workspace design.
-
-Keep in mind that workplace strategy should align with your organization's unique goals and culture.
+Workplace Strategy is the art and science of aligning an organisation's people with their physical work environment(s) to improve organisational performance. It is led by business outcomes, not design. It is the foundation for workplace transformation. It works by aligning people outcomes (such as behaviours and perceptions) and translating these into a recipe for the design and operation of their workplace environments.
+It includes: management consulting, workforce discovery activities, workspace analytics, occupancy studies, clarification of the future state, leadership alignment, design requirements and technology requirements.
+It does not include: Interior design, Architecture, Project Management, Technology expertise, Organisational Development or Organisational Psychology.
+Change management supports Workplace Strategy by helping to deliver on the people outcomes. It includes capability building, influencing, alignment and change leadership. It does not include Interior design, Architecture, Project Management, Technology expertise, Organisational Development or Organisational Psychology.
 """
 
 def generate_response(prompt):
